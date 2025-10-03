5390174c6f58746729f6aebb4d75f0972e229d54
Stéphane <stephane.ahmedboulanger@gmail.com>
2025-10-03 09:48:10 +0200

    maj exo1 iteratives

--- python_iterables/exo1.py
+++ python_iterables/exo1.py
@@ -4,13 +4,32 @@
 
 # Given a list [1, 2, 3, 4], print out all the values in the list one by one.
 
+number_list = [1, 2, 3, 4]
+print(number_list)
+
 # Given a list [1, 2, 3, 4], print out all the values in the list multiplied by 20.
+
+number_list = [1, 2, 3, 4]
+for i in number_list:
+    print(i * 20)
 
 # Given a list ["Elie", "Tim", "Matt"], return a new list with only the first letter of each name: ["E", "T", "M"].
 
-# Given a list [1, 2, 3, 4, 5, 6], return a new list with all the even values: [2, 4, 6].
+name_list = ["Elie", "Tim", "Matt"]
+for i in name_list:
+    print(i[0])
+
+# Given a list [1, 2, 3, 4, 5, 6], return a new list with all the even values: [2, 4, 6]
+
+number_list = [1, 2, 3, 4, 5, 6]
+even_numbers = [i for i in number_list if i % 2 == 0]
+print(even_numbers)
 
 # Given two lists [1, 2, 3, 4] and [3, 4, 5, 6], return a new list that contains only the values present in both lists: [3, 4].
+number_list1 = [1, 2, 3, 4]
+number_list2 = [3, 4, 5, 6]
+common = list(set(number_list1) & set(number_list2))
+print(common)
 
 # Given a list of words ["Elie", "Tim", "Matt"], return a new list with each word reversed and in lowercase: ["eile", "mit", "ttam"].
 
