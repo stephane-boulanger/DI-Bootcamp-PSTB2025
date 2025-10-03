84b0f8594c212b36b5c0e7fba95b6304173ad92e
Stéphane <stephane.ahmedboulanger@gmail.com>
2025-10-03 07:26:24 +0200

    exo2

--- python_intro/exo2.py
+++ python_intro/exo2.py
@@ -1,31 +1,43 @@
-#1. Write a Python expression to calculate the sum of 10 and 3
-sum = 10 + 3
-print(sum)
+# Instructions
+# Write a program that will calculate cat’s and dog’s years based on human years:
 
-#2. Subtract 7 from 15 using Python
-sub = 15 - 7
-print(sub)
+# I have a cat and a dog. I got them at the same time as kitten/puppy. That was humanYears years ago. Print their respective ages now as [humanYears,catYears,dogYears]
 
-#3. Multiply 4 by 8 using Python
-mul = 4 * 8
-print(mul)
+# NOTES:
 
-#4. Divide 18 by 3
-div = 18 / 3
-print(div)
+# humanYears >= 1 humanYears are whole numbers only
 
-#5. Use floor division to find how many whole times 11 can be divided by 4
-fl_div = 11 // 4
-print(fl_div)
+# Cat Years 15 cat years for first year +9 cat years for second year +4 cat years for each year after that
 
-#6. Find the remainder when 9 is divided by 4 using the modulus operator
-mod = 9 % 4
-print(mod)
+# Dog Years 15 dog years for first year +9 dog years for second year +5 dog years for each year after that
 
-#7. Raise 6 to the power of 3
-pow = 6 ** 3
-print(pow)
+# example of output:
 
-#8. Use the negation operator to make the number 12 negative
-neg = 3 - 6
-print(neg)
+# human_years = 10
+# #output: [10, 56, 64]
+
+# human_years = 1
+# #output: [1, 15, 15]
+
+# human_years = 2
+# #output [2, 24, 24]
+
+
+# test the program with the following values:
+
+# human_years = 10
+# human_years = 1
+# human_years = 2
+
+def cat_dog_years(human_years):
+    if human_years == 1:
+        return [1, 15, 15]
+    elif human_years == 2:
+        return [2, 24, 24]
+    else:
+        cat = 24 + (human_years - 2) * 4
+        dog = 24 + (human_years - 2) * 5
+        return [human_years, cat, dog]
+
+for y in (10, 1, 2):
+    print(cat_dog_years(y))
